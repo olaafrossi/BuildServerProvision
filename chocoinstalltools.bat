e7f5d1e97e3ba6ef9d7c2a7cda717374352645e5
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-01-30 05:33:52 -0500

    Update chocoinstalltools.bat
    adds ref to blackmagic installer for Clear Channel CMS

--- chocoinstalltools.bat
+++ chocoinstalltools.bat
@@ -30,3 +30,6 @@ choco install nodejs.install -fy
 choco install wget -fy
 choco install rsync -fy
 choco install microsoft-windows-terminal -fy
+
+:: manually install blackmagic ATEM software for the CLear Channel CMS COM reference 
+:: https://sw.blackmagicdesign.com/ATEM/v6.3/Blackmagic_ATEM_Switchers_Windows_6.3.zip?Key-Pair-Id=APKAJTKA3ZJMJRQITVEA&Signature=UItZGwYwv3m9XCwvI2f7amxxpQpcQwn3BLbMhxgH/ReH1qDX/plwNiFYtmTA1dvX9OhkVxqXYw4gbeIr+QHlrFqQTeH2z8jLCQh0yJco32FN/24XJrdtVWs3pBHILvSblttogCzawGKTSB3JdW0t/niRgR3RkjilDAPg+b2aIsEPPgc9XC9cq2sEEWuQNeBvoH/iFnn+TJ0lw9UD5MB8OModuI6XWA45mak2sRrRVr8blVtSXdEK017AmsM/jisMff1VOD8sIt2EnFjzTYU+5FXl94BSgRT5jzui8U3eWfYQZO7DmwbVmZvRKlxK56cQWmqhjXBnhkO2GcIsPqlubg==&Expires=1612012923
